c376e69a579c8161443b9fb9e8a2e2bf679037a2
Ishan Tharindu <t.gunawardana864@gmail.com>
2024-09-10 16:35:11 +0530

    change env variable name

--- server/utils/dbConnection.js
+++ server/utils/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.log(err);
